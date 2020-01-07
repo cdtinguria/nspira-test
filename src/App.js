39d4990f8ca0b10c05b9c33cb1eb38d12b9c1fff
Chetan Tinguria <chetan.tinguria@gmail.com>
2020-01-08 01:06:28 +0530

    moving to class based component to integrate redux

--- src/App.js
+++ src/App.js
@@ -4,55 +4,126 @@ import "./App.css";
 
 import MovieCard from "./MovieCard";
 
-function App() {
-  let [movies, setMovies] = React.useState([]);
-  let [pageCount, setPageCount] = React.useState(1);
-  let [fetching, setFetching] = React.useState(false);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      pageCount: 1,
+      fetching: false,
+      error: false
+    };
 
-  async function fetchData() {
-    let mvs = await (
-      await fetch(`https://akrp-server.herokuapp.com/movies?p=${pageCount}`)
-    ).json();
-
-    setFetching(false);
-    setMovies([...movies, ...mvs]);
+    window.addEventListener("scroll", this.onScroll);
   }
 
-  function onScroll() {
+  async componentDidMount() {
+    this.setState({
+      fetching: true
+    });
+
+    let mvs = await (
+      await fetch(
+        `https://akrp-server.herokuapp.com/movies?p=${this.state.pageCount}`
+      ).catch(() =>
+        this.setState({
+          error: true,
+          fetching: false
+        })
+      )
+    ).json();
+    
+    this.setState({
+      movies: [...this.state.movies, ...mvs],
+      fetching: false,
+      error: false
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  }
+
+  onScroll() {
     if (
       window.innerHeight + document.documentElement.scrollTop ===
       document.documentElement.offsetHeight
     ) {
-      setPageCount(pageCount + 1);
+      this.setState({
+        pageCount: this.state.pageCount + 1
+      });
     }
   }
 
-  React.useEffect(() => {
-    setFetching(true);
-    fetchData();
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, [pageCount]);
+  render() {
+    let movieCards = [];
+    for (let _m of this.state.movies) {
+      movieCards.push(<MovieCard movie={_m} key={_m._id} />);
+    }
 
-  let movieCards = [];
-  for (let _m of movies) {
-    movieCards.push(
-      <MovieCard movie={_m} key={_m._id} />
+    if (this.state.fetching)
+      movieCards.push(
+        <img src={logo} className="App-logo" alt="logo" key="fetching" />
+      );
+
+    return (
+      <div className="container">
+        <div className="row padded">{movieCards}</div>
+      </div>
     );
   }
-
-  if (fetching)
-    movieCards.push(
-      <img src={logo} className="App-logo" alt="logo" key="fetching" />
-    );
-
-  return (
-    <div className="container">
-      <div className="row padded">{movieCards}</div>
-    </div>
-  );
 }
+
+// below is using React Hooks...
+// function App() {
+//   let [movies, setMovies] = React.useState([]);
+//   let [pageCount, setPageCount] = React.useState(1);
+//   let [fetching, setFetching] = React.useState(false);
+
+//   async function fetchData() {
+//     let mvs = await (
+//       await fetch(`https://akrp-server.herokuapp.com/movies?p=${pageCount}`)
+//     ).json();
+
+//     setFetching(false);
+//     setMovies([...movies, ...mvs]);
+//   }
+
+//   function onScroll() {
+//     if (
+//       window.innerHeight + document.documentElement.scrollTop ===
+//       document.documentElement.offsetHeight
+//     ) {
+//       setPageCount(pageCount + 1);
+//     }
+//   }
+
+//   React.useEffect(() => {
+//     setFetching(true);
+//     fetchData();
+//     window.addEventListener("scroll", onScroll);
+//     return () => {
+//       window.removeEventListener("scroll", onScroll);
+//     };
+//   }, [pageCount]);
+
+//   let movieCards = [];
+//   for (let _m of movies) {
+//     movieCards.push(
+//       <MovieCard movie={_m} key={_m._id} />
+//     );
+//   }
+
+//   if (fetching)
+//     movieCards.push(
+//       <img src={logo} className="App-logo" alt="logo" key="fetching" />
+//     );
+
+//   return (
+//     <div className="container">
+//       <div className="row padded">{movieCards}</div>
+//     </div>
+//   );
+// }
 
 export default App;
